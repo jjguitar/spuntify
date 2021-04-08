682760dba094398febef5d894ec692c6a4ba4087
jjguitar <jhon.electronica@gmail.com>
2021-04-07 19:27:32 -0500

    Random generator cards colors

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -1,49 +1,25 @@
+function cardGenerator(n) {
+  let block = ''
+
+  for(let i = 0; i < n; i++) {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    block = block + '\n' +
+    `<div class="cards__top-item">
+      <div style="background-color:#${randomColor};"></div>
+      <p>Easter/Día de Resurrección</p>
+    </div>`
+  }
+  return block
+}
+
 const Main = () => {
   const view = `
   <h1 class="content__tittle-top">Good evening</h1>
   <div class="cards__top">
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
+    ${cardGenerator(4)}
   </div>
   <div class="cards__top">
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
-    <div class="cards__top-item">
-      <div></div>
-      <p>Easter/Día de Resurrección</p>
-    </div>
+    ${cardGenerator(2)}
   </div>
   `;
   return view;
