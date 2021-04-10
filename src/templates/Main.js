bbb0dfcebb9e2a51602d6a3816864c464163f065
jjguitar <jhon.electronica@gmail.com>
2021-04-10 00:04:56 -0500

    add recent list

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -2,11 +2,32 @@ function cardGenerator(n) {
   let block = ''
 
   for(let i = 0; i < n; i++) {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
     block = block + '\n' +
     `<div class="cards__top-item">
-      <div style="background-color:#${randomColor};"></div>
+      <div style="background-color:#${randomColor()};"></div>
       <p>Easter/Día de Resurrección</p>
+    </div>`
+  }
+  return block
+}
+
+function randomColor() {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function cardGeneratorList(n) {
+  let block = ''
+
+  for(let i = 0; i < n; i++) {
+    block = block + '\n' +
+    `<div class="card__list">
+      <h2>Recently played</h2>
+      <div class="test">
+        <div style="background-color:#${randomColor()};">
+        </div>
+        <h1>Release Radar</h1>
+        <p>Catch all the latest music from artists you follow, plus mew...</p>
+      </div>
     </div>`
   }
   return block
@@ -19,7 +40,10 @@ const Main = () => {
     ${cardGenerator(4)}
   </div>
   <div class="cards__top">
-    ${cardGenerator(2)}
+    ${cardGenerator(6)}
+  </div>
+  <div class="cards__container-list">
+    ${cardGeneratorList(3)}
   </div>
   `;
   return view;
--- src/templates/NavRight.js
+++ src/templates/NavRight.js
@@ -12,13 +12,13 @@ const NavRight = () => {
       <img src="${logo}" alt="logo">
     </div>
     <ul class="nav-right__home">
-      <li class="gray__push"><img src="${home}" class="icon__home">Home</li>
+      <li class="gray__push"><img src="${home}" class="icon__home" width="20" height="20">Home</li>
       <li><img src="${search}" class="icon__home">Search</li>
       <li><img src="${library}" class="icon__home">Your Library</li>
     </ul>
     <ul class="nav-right__create-playlist">
-      <li><img src="${plus}" class="icon__home plus">Create Playlist</li>
-      <li><img src="${heart}" class="icon__home heart">Liked Songs</li>
+      <li><img src="${plus}" alt="plus" class="icon__home plus">Create Playlist</li>
+      <li><img src="${heart}" alt="heart" class="icon__home heart">Liked Songs</li>
     </ul>
     <ul class="nav-right__top-list">
       <li>Franca Pizarra</li>
