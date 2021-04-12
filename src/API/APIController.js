2870e912abdec65abbcbf0f3bb1e13125b3611f1
jjguitar <jhon.electronica@gmail.com>
2021-04-11 23:26:39 -0500

    Add API

---
+++ src/API/APIController.js
@@ -0,0 +1,86 @@
+const APIController = (function() {
+
+  const clientId = '0b5d68ec183745c88d11fefac1c2ae1c';
+  const clientSecret = '429ee0e54a514f86b5270c07644df3c2';
+
+  // private methods
+
+  const setCookie = (name, value, days) => {
+    let expires = "";
+    if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  };
+
+  const _getToken = async () => {
+
+      const result = await fetch('https://accounts.spotify.com/api/token', {
+          method: 'POST',
+          headers: {
+              'Content-Type' : 'application/x-www-form-urlencoded', 
+              'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+          },
+          body: 'grant_type=client_credentials'
+      });
+
+      const data = await result.json();
+      setCookie('BearerToken', data.access_token, 30)
+  }
+
+  const _getGenres = async (token) => {
+
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json();
+    return data.categories.items;
+  }
+
+  const _getPlaylistByGenre = async (token, genreId) => {
+
+    const limit = 12;
+
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json();
+    return data.playlists.items;
+  }
+
+  const _getTracks = async (token, tracksEndPoint) => {
+
+    const limit = 7;
+
+    const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json();
+    return data.items;
+  }
+
+  return {
+    getToken() {
+        return _getToken();
+    },
+    getGenres(token) {
+        return _getGenres(token);
+    },
+    getPlaylistByGenre(token, genreId) {
+      return _getPlaylistByGenre(token, genreId);
+    },
+    getTracks(token, tracksEndPoint) {
+      return _getTracks(token, tracksEndPoint);
+    },
+  }
+})();
+
+export default APIController;
--- src/templates/Main.js
+++ src/templates/Main.js
@@ -1,62 +1,108 @@
-function cardGenerator(n) {
-  let block = ''
+import APIController from '../API/APIController.js'
+
+function cardGenerator(playlist) {
+  let n = playlist.length
+  let block1 = ''
+  let block2 = ''
+  // console.log(playlist)
 
   for(let i = 0; i < n; i++) {
-    block = block + '\n' +
+    if (i < n / 2) {
+      block1 = block1 + '\n' +
     `<div class="cards__top-item">
-      <div style="background-color:#${randomColor()};"></div>
-      <p>Easter/Día de Resurrección</p>
+      <div>
+        <img src="${playlist[i].images[0].url}" width="80">
+      </div>
+      <p>${playlist[i].name}</p>
     </div>`
+    } else {
+      block2 = block2 + '\n' +
+    `<div class="cards__top-item">
+      <div>
+        <img src="${playlist[i].images[0].url}" width="80">
+      </div>
+      <p>${playlist[i].name}</p>
+    </div>`
+    }
   }
-  return block
+
+  return `
+    <div class="row__1">
+      ${block1}
+    </div>
+    <div class="row__1">
+      ${block2}
+    </div>
+  `
 }
 
 function randomColor() {
   return Math.floor(Math.random()*16777215).toString(16);
 }
 
-function cardGeneratorList(n) {
-  let block = ''
-
-  for(let i = 0; i < n; i++) {
-    block = block + '\n' +
-    `<div class="card-recently">
-      <div class="card-img" style="background-color:#${randomColor()};">
-      </div>
-      <h1>Tittle of list</h1>
-      <p>By Erick Lugo</p>
-    </div>`
+const getCookie = name => {
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
-  return block
+  return null;
+};
+
+function generatorCards(tracks) {
+  let block = ''
+  // console.log(tracks)
+  for (let i of tracks) {
+    // console.log(i.track.album.artists[0].name)
+    block = block + '\n' +
+    `
+        <div class="card-recently">
+          <div class="card-img" style="background-color:#${randomColor()};">
+            <img src="${i.track.album.images[1].url}" alt="img" width="160">
+          </div>
+          <h1>${i.track.name}</h1>
+          <p>By ${i.track.album.artists[0].name}</p>
+        </div>
+    `
+  }
+
+  return `
+  <div class="container__recently">
+    ${block}
+  </div>
+  `
 }
 
-const Main = () => {
+const cardGeneratorList = async (playlist) => {
+  let n = playlist.length
+  let blockSection = ''
+
+  for(let i = 1; i < n / 2; i++) {
+    let endPoint = playlist[i].tracks.href
+    let tracks = await APIController.getTracks(getCookie('BearerToken'), endPoint)
+    blockSection = blockSection + '\n' +
+    `
+    <section class="recently-played">
+      <h2>${playlist[i].name}</h2>
+      ${generatorCards(tracks)}
+    </section>
+    `
+  }
+  return blockSection
+}
+
+const Main = async (playlist) => {
   const view = `
   <div class="container-main">
     <section class="greetings">
       <h1 class="content__tittle-top">Good evening</h1>
       <div class="container__greeting">
-        <div class="row__1">
-          ${cardGenerator(4)}
-        </div>
-        <div class="row__1">
-          ${cardGenerator(4)}
-        </div>
+        ${cardGenerator(playlist)}
       </div>
     </section>
-    <section class="recently-played">
-      <h2>Recently played</h2>
-      <div class="container__recently">
-        ${cardGeneratorList(5)}
-      </div>
-    </section>
-    <section class="recently-played">
-      <h2>Recently played</h2>
-      <div class="container__recently">
-        ${cardGeneratorList(5)}
-      </div>
-    </section>
-  </div>
+    ${await cardGeneratorList(playlist)}
   `;
   return view;
 };
